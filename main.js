58dcc704f33d006336a01a2da317d3becee1cbdf
bilal <m-bilaljaved612@gmail.com>
2024-08-01 12:23:30 +0500

    add some bugs

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 
 // for nav link
 
-const menunavicon= document.getElementById("menunavicon");
-const navlinks= document.getElementById("navlinks");
+const menunavicon = document.getElementById("menunavicon");
+const navlinks = document.getElementById("navlinks");
 
-menunavicon.addEventListener("click", ()=>{
+menunavicon.addEventListener("click", () => {
 
     navlinks.classList.toggle("navlinks-active")
 })
@@ -13,19 +13,19 @@ menunavicon.addEventListener("click", ()=>{
 
 
 // for tabs link 
-        function openCity(evt, tabName) {
-            var i, tabcontent, tablinks;
-            tabcontent = document.getElementsByClassName("tabcontent");
-            for (i = 0; i < tabcontent.length; i++) {
-                tabcontent[i].style.display = "none";
-            }
-            tablinks = document.getElementsByClassName("tablinks");
-            for (i = 0; i < tablinks.length; i++) {
-                tablinks[i].className = tablinks[i].className.replace(" active", "");
-            }
-            document.getElementById(tabName).style.display = "block";
-            evt.currentTarget.className += " active";
-        }
+function openCity(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
 
-        // Get the element with id="defaultOpen" and click on it
-        document.getElementById("defaultOpen").click();
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
