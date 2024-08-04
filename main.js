574b53bb5fa88200df2cddc19652443fbd3cf5ec
bilal <m-bilaljaved612@gmail.com>
2024-08-04 16:59:01 +0600

    add animation in website

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const serachbar = document.getElementById("serachbar");
 
 serachbaricon.addEventListener("click", () => {
 
-    serachbar.classList.toggle("serachbar-active")
+  serachbar.classList.toggle("serachbar-active")
 })
 // for nav link menu
 
@@ -15,7 +15,7 @@ const navlinks = document.getElementById("navlinks");
 
 menunavicon.addEventListener("click", () => {
 
-    navlinks.classList.toggle("navlinks-active")
+  navlinks.classList.toggle("navlinks-active")
 })
 
 
@@ -23,18 +23,41 @@ menunavicon.addEventListener("click", () => {
 
 // for tabs link 
 function openCity(evt, tabName) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(tabName).style.display = "block";
-    evt.currentTarget.className += " active";
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
 }
 
 // Get the element with id="defaultOpen" and click on it
 document.getElementById("defaultOpen").click();
+
+
+
+
+
+// for animation 
+
+document.addEventListener('DOMContentLoaded', () => {
+  const elements = document.querySelectorAll('.opac');
+
+  function checkVisibility() {
+    elements.forEach(el => {
+      const rect = el.getBoundingClientRect();
+      if (rect.top < window.innerHeight && rect.bottom > 0) {
+        el.classList.add('animate__animated', el.getAttribute('data-animation'));
+        el.classList.remove('opac');
+      }
+    });
+  }
+
+  window.onscroll = checkVisibility;
+  checkVisibility();  // Check visibility on load in case some elements are already in view
+});
